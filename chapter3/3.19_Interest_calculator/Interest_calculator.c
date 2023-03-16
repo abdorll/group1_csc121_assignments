09038e4c83a4f274e1dcd594ae9ed8471d4764a7
adedamola-toye <toye.adedamola@gmail.com>
2023-03-16 12:53:54 +0100

    Toyibah corrected 3.19&4.19

--- chapter3/3.19_Interest_calculator/Interest_calculator.c
+++ chapter3/3.19_Interest_calculator/Interest_calculator.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
 int main(void){
+
+    //VARIABLE DECLARATION AND INITIALIZATION PHASE
     float principal,interestCharge,rate =0.0;
     int days;
     interestCharge=0;
+
     printf("Enter loan principal (-1 to end) : "); 
     scanf("%f", &principal);
+
     while (principal !=-1){
+
         printf("Enter interest rate : ");
         scanf("%f", &rate);
+
         printf("Enter term of the loan in days : ");
         scanf("%d", &days);
+
         interestCharge = principal * rate * days/365;
-        printf("The interest charge is $%.2f\n", interestCharge);
+
+        printf("The interest charge is $%.2f\n\n", interestCharge);
+
         printf("Enter loan principal (-1 to end) : ");
         scanf("%f", &principal);
-    }
-}
+    }//End while loop
+} //End main function
--- chapter4/4.19_calculating_sales/calculating_sales.c
+++ chapter4/4.19_calculating_sales/calculating_sales.c
@@ -1,47 +1,64 @@
-#include <stdio.h> 
+
+#include <stdio.h>
+
 int main(void){
+
+    //VARIABLE DECLARATION AND INITIALIZATION PHASE
     int productNo, day, quantitySoldPerDay;
     float totalRetailValue = 0;
     float retailValuePerDay = 0;
     float retailValue, retailPrice;
-        for(day=1; day<=7; day++){
-            retailValuePerDay=0;
-            printf("DAY %d: \n", day);
-            printf("Enter product number (-1 to end):  ");
-            scanf("%d", &productNo);
-            while(productNo != -1){
+
+    for(day=1; day<=7; day++){ //For each day of the week
+        retailValuePerDay=0;
+        printf("DAY %d: \n", day);
+        printf("Enter product number (-1 to end):  ");
+        scanf("%d", &productNo);
+            
+        while(productNo != -1){
+
+            if(productNo >=1 && productNo <=5){ 
                 switch(productNo){
                     case 1:
                         retailPrice = 2.98;
                         break;
+
                     case 2:
                         retailPrice = 4.50;
                         break;
+                        
                     case 3:
                         retailPrice = 9.98;
                         break;
+                        
                     case 4:
                         retailPrice = 4.49;
                         break;
+                        
                     case 5:
                         retailPrice = 6.87;
                         break;
-                    default:
-                        puts("🛑❗PRODUCT NUMBER MUST BE WITHIN 1-5❗🛑");
-                        retailPrice = 0;
-                        printf("Enter another product number between from 1 to 5: ");
-                        scanf("%d", &productNo);
-                        break;
-                }
+                } //End switch
+                
                 printf("Enter quantity sold for one day: ");
                 scanf("%d", &quantitySoldPerDay);
+                
                 retailValue = retailPrice * quantitySoldPerDay;
                 retailValuePerDay+=retailValue;
-                printf("Enter product number (-1 to end): ");
-                scanf("%d", &productNo);
-            }
-     printf("Total retail value for day %d : $%.2f\n\n",day, retailValuePerDay);
-    totalRetailValue+=retailValuePerDay;
-    }
+            } //End if 
+
+            else{
+                printf("\nProduct %d is not available!\n", productNo);
+                puts("PRODUCT MUST BE FROM 1 TO 5!\n");
+            } //End else
+            
+            printf("Enter product number (-1 to end): ");
+            scanf("%d", &productNo);
+        } //End while loop
+            printf("Total retail value for day %d : $ %.2f\n\n",day, retailValuePerDay);
+    
+        totalRetailValue+=retailValuePerDay;
+    } //End for loop
     printf("\nTotal retail value for one week : $ %.2f", totalRetailValue);
 }
+
