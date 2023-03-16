c9f51efc990874d572ea9cd72b3e056f64518066
abdorll <abdullahopadeji50@.com>
2023-03-16 16:05:24 +0100

    pushing concluded1 to develop

--- chapter5/5.11_rounding_numbers/rounding_numbers.c
+++ chapter5/5.11_rounding_numbers/rounding_numbers.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-double x,y=0;
-int main(){
-    //y = floor(x * 100 + .5) / 100;
-    printf("Enter x: ");
-    scanf("%lf",&x);
-    while (x!=-1){
-    y = floor(x*10 + 0.5)/10;
-    printf("floor  = %lf",y);
-    puts("");
-    printf("\nEnter x: ");
-    scanf("%lf",&x);
+
+double roundToInteger(double number) {
+    return floor(number + 0.5); // rounds a number to integer
+}
+double roundToTenths(double number) {
+    return floor(number * 10 + 0.5) / 10; //rounds-up to the nearest 10th
+}
+double roundToHundredths(double number) {
+    return floor(number * 100 + 0.5) / 100;  //rounds-up to the nearest 100th
+}
+double roundToThousandths(double number) {
+    return floor(number * 1000 + 0.5) / 1000;  //rounds-up to the nearest 1000th
+}
+double x; // declare c
+int counter=0; // initialize counter
+void askForX(){
+counter++; //increment counter
+printf("ROUND NUMBER #%d\n", counter);
+ printf("Enter a number (-1 to quit): ");
+        scanf("%lf", &x); // asks user input (x)
+}
+int main() {
+    askForX();
+    while(x!=-1){
+        printf("Original value: %lf\nRounded to integer: %f\nRounded to tenths: %f\nRounded to hundredths: %f\nRounded to thousandths: %f\n\n",
+     x,roundToInteger(x),roundToTenths(x), roundToHundredths(x),roundToThousandths(x));
+    askForX();
     }
     return 0;
 }
--- chapter5/5.15_hypotenus_calculations/hypotenus_calculations.c
+++ chapter5/5.15_hypotenus_calculations/hypotenus_calculations.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-#include <math.h>
-double opposite, adjacent, hypotenuse;
-void determineHypotenuse(double opposite, double adjacent){
-hypotenuse = sqrt(pow(opposite,2) + pow(adjacent,2));
-opposite<=0||adjacent<=0? puts("Enter valid lengths"): printf("Hypotenuse is: %.2lf \n", hypotenuse);
+#include<math.h>
+
+double hypotenuse(double opposite, double adjacent){ 
+    double hypotenuse = sqrt(pow(opposite, 2) + pow(adjacent, 2)); // func. to return hypotenuse
+return hypotenuse;
 }
-int index =0;
-void input1(){
-    index++;
-    printf("TRIANGLE #%d\nEnter side #1 (-1 to cancel): ", index);
-    scanf("%lf", &adjacent);}
-int main() {
-    input1();
-    while(adjacent!=-1){
-    printf("Enter side #2: ");
-    scanf("%lf", &opposite);
-    determineHypotenuse(adjacent, opposite);
+double side1, side2; // declare the real 2  sides of the triangle (ie side1 & side2)
+int counter = 0; //initialize counter to 0
+void firstSideOfTriangle(){ //func. to get first input (ie the adj./opp. ie 1 of the sides) starting with printing TRIANGLE #num
+    counter++;
+    printf("TRIANGLE #%d\n", counter);
+    printf("Enter side #1 in (m) (-1 to cancel): ");
+    scanf("%lf", &side1);
+}
+int main(){
+    firstSideOfTriangle(); //asks user for 1st input (side1)
+    while(side1 != -1){
+    printf("Enter side #2 in (m): "); //asks user for 1st input (side2)
+    scanf("%lf", &side2);
+    side1<=0||side2<=0  // checks if the 2 side are valid (ie are not <=0)
+    ? puts("Error!! Both lengths must be valid!") // if not valid; prints err. mssg.
+    : printf("Hypotenuse is: %.2f m.\n", hypotenuse(side1, side2)); // else (ie, it's valid and prints the hypotenuse)
     puts("");
-    input1();
-    }
-    return 0;
+    firstSideOfTriangle();
+    }    
 }
