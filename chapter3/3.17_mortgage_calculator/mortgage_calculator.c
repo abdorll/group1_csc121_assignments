f2efb35f4261a219fa1e00988840b9efad2ac134
Emzard <87395745+Emzard@users.noreply.github.com>
2023-03-16 12:10:47 +0100

    Made some changes and added more whitespace

--- chapter3/3.17_mortgage_calculator/mortgage_calculator.c
+++ chapter3/3.17_mortgage_calculator/mortgage_calculator.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main (void)
 {
     int accountNumber,mortgageAmount,mortgageTerm=0;
     float interestRate;
-printf("%s" ,"Enter account number (-1 to end): ");
-        scanf("%d", &accountNumber);
+    
+    printf("%s" , "Enter account number (-1 to end): ");
+    scanf("%d", &accountNumber);
+    
     while ( accountNumber !=-1)
     {
-        printf("%s" ,"Enter mortgage amount (in dollars): ");
+        printf("%s", "Enter mortgage amount (in dollars): ");
         scanf("%d", &mortgageAmount);
-        printf("%s" ,"Enter mortgage term (in years): ");
+        
+        printf("%s", "Enter mortgage term (in years): ");
         scanf("%d", &mortgageTerm);
-        printf("%s" ,"Enter interest rate (as a decimal): ");
+        
+        printf("%s", "Enter interest rate (as a decimal): ");
         scanf("%f", &interestRate);
+        
         float totalInterest = mortgageAmount * interestRate * mortgageTerm;
         float totalAmount = mortgageAmount + totalInterest;
         float monthlyInterest = totalAmount / (mortgageTerm * 12);
-        printf("The monthly payable interest is $ %1.f\n", monthlyInterest);
-        printf("%s" ,"Enter account number (-1 to end): ");
+        
+        printf("The monthly payable interest is $ %.0f\n", monthlyInterest);
+        puts("");
+        
+        printf("%s", "Enter account number (-1 to end): ");
         scanf("%d", &accountNumber);
-
 
     }
 
